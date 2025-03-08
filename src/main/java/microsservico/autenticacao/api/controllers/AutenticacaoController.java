b138535737812abd2a836cd84a9f5286ea36a46b
João Secundo <jops40@hotmail.com>
2025-03-08 16:09:36 -0300

    Métrica personalizada de autenticações com sucesso ou falha

--- src/main/java/microsservico/autenticacao/api/controllers/AutenticacaoController.java
+++ src/main/java/microsservico/autenticacao/api/controllers/AutenticacaoController.java
@@ -4,10 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import microsservico.autenticacao.api.domain.models.LoginDTO;
 import microsservico.autenticacao.api.domain.models.Usuario;
 import microsservico.autenticacao.api.infra.security.TokenService;
@@ -27,14 +31,31 @@ public class AutenticacaoController {
     public ResponseEntity<TokenDto> login(@RequestBody LoginDTO loginDto) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginDto.email(), loginDto.senha());
 
-        Authentication autenticacao = authManager.authenticate(authToken); // vai usar o BCrypt, pois foi configurado SecurityConfig.java
-
-        String tokenJWT = tokenService.gerarTokenJWT((Usuario) autenticacao.getPrincipal());
-
-        return ResponseEntity.ok(new TokenDto(tokenJWT));
+        try {
+            Authentication autenticacao = authManager.authenticate(authToken); // vai usar o BCrypt, pois foi configurado SecurityConfig.java
+            String tokenJWT = tokenService.gerarTokenJWT((Usuario) autenticacao.getPrincipal());
+            authSuccesses.increment(); // Incrementar métrica personalizada de autenticações com sucesso
+            return ResponseEntity.ok(new TokenDto(tokenJWT));
+        } catch (AuthenticationException e) {
+            authFails.increment(); // Incrementar métrica personalizada de autenticações com falha
+            throw e; // o TratadorDeExceptions vai lidar com a exception. (Talvez seja melhor colocar a métrica de falha lá)
+        }
     }
 
     private record TokenDto (String token) {}
+
+    // Métricas personalizadas de autenticações com sucesso ou falha
+    Counter authSuccesses;
+    Counter authFails;
+    public AutenticacaoController(MeterRegistry registry_de_metricas) { // injetar o registry no controller
+        authSuccesses = Counter.builder("auth_user_sucess")
+            .description("Autenticações com sucesso")
+            .register(registry_de_metricas);
+            
+        authFails = Counter.builder("auth_user_fails")
+            .description("Autenticações com falha")
+            .register(registry_de_metricas);
+    }
 }
 
 
